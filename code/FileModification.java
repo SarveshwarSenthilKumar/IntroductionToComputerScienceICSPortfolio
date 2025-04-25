5add866610450a7eafc8676266bd26845e6a3bbc
Sarveshwar Senthil Kumar <47092652+SarveshwarSenthilKumar@users.noreply.github.com>
2025-04-25 07:38:20 -0400

    Updated file modification lab

--- code/FileModification.java
+++ code/FileModification.java
@@ -59,11 +59,43 @@ public class FileModification {
         
         /* 
         //Render Random Student Marks
-        int RandomMark = (int)(Math.random()*60) + 40;
         PrintWriter writer = new PrintWriter(new java.io.FileWriter("code/marks.txt", true));
-        writer.println("Student: " + RandomMark);
+        for (int i = 0; i < 100; i++){
+            int RandomMark = (int)(Math.random()*60) + 40;
+            writer.println("Student:" + RandomMark);
+        }
         writer.close();
         */
+
+         /* 
+
+        //Read Random Student Marks
+        BufferedReader sentenceReader = new BufferedReader(new java.io.FileReader("code/sentences1.txt"));
+        String currentLine = sentenceReader.readLine();
+        int totalMarks = 0;
+        int numberofStudents = 0;
+        int maximumMarks = 0;
+        int minimumMarks = 100;
+        while (currentLine != null) {
+            int mark = Integer.parseInt(currentLine.split(":")[1]);
+            if (mark > maximumMarks) {
+                maximumMarks = mark;
+            }
+            if (mark < minimumMarks) {
+                minimumMarks = mark;
+            }
+            totalMarks+=mark;
+            numberofStudents++;
+            currentLine = sentenceReader.readLine();
+        }
+
+        System.out.println("The average marks of the students is: " + (double)totalMarks/numberofStudents);
+        System.out.println("The maximum marks of the students is: " + maximumMarks);
+        System.out.println("The minimum marks of the students is: " + minimumMarks);
+        System.out.println("The range of marks of the students is: " + (maximumMarks - minimumMarks));
+
+        */
+
 
         /* 
         // Read and print the average length of a sentence
