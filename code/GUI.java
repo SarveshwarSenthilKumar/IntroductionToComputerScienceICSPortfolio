627c1a7e7365c18aecb6ae9a31d16695eb6d010b
Sarveshwar Senthil Kumar <47092652+SarveshwarSenthilKumar@users.noreply.github.com>
2026-04-22 13:51:23 -0400

    Sample GUI code for Java Swing application

---
+++ code/GUI.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GUI {
+    public static void main(String[] args) {
+        // Create a JFrame
+        JFrame frame = new JFrame("My Swing Application");
+
+        //Create a panel
+        JPanel contents = new JPanel();
+        contents.setBackground(Color.yellow);
+
+        //create other components
+        JLabel label = new JLabel("Hello, Swing!");
+        JButton myButton = new JButton("Click Me");
+        JTextField text = new JTextField(30);
+
+        //Set contentPane
+        contents.add(label);
+        contents.add(myButton);
+        contents.add(text);
+
+        // Size and display the frame
+        frame.setContentPane(contents);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();;
+        frame.setLocationRelativeTo(null); // Center on screen
+        frame.setVisible(true);
+
+    }
+}
